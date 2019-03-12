e9905deaf495fd63a5bc1753e9674e4acfb52798
shy <346250026@qq.com>
2019-03-12 08:55:10 +0800

    new index

--- 0312.sql
+++ 0312.sql
@@ -6,3 +6,10 @@ drop index idx_ename on scott.emp;
 
 show index from db_school.student;
 
+-- 如果index只有好处 ，没有必要 存在这个概念
+create index ind_password on db_csdn.user(password);
+select password, count(password)
+from db_csdn.user
+group by password
+order by 2 desc   -- 按所选的第二列进行排序
+limit 10 offset 0;
